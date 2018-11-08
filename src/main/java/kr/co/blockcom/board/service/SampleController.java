701a22aef76902943e2b1e0808fec8913003574e
seokbae <seokbae@blockcom.co.kr>
2018-11-08 16:25:55 +0900

    ajax sample 추가

--- src/main/java/kr/co/blockcom/board/service/SampleController.java
+++ src/main/java/kr/co/blockcom/board/service/SampleController.java
@@ -1,8 +1,11 @@
 package kr.co.blockcom.board.service;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/showMessage")
@@ -14,5 +17,10 @@ public class SampleController {
 	@GetMapping
 	public String list() throws Exception{
 		return "/showMessage";
+	}
+	
+	@RequestMapping(value = {"/xhr"}, method = RequestMethod.GET)
+	public ResponseEntity<String> listAjax(){
+		return new ResponseEntity<String>("", HttpStatus.OK);
 	}
 }
