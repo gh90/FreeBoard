f7de7309e72f18566dbbb93f18800f6b60702bee
kgh <rmsgud0202@blockcom.co.kr>
2018-11-16 15:09:47 +0900

    merge

--- src/main/java/kr/co/blockcom/board/biz/boardfree/service/BoardFreeService.java
+++ src/main/java/kr/co/blockcom/board/biz/boardfree/service/BoardFreeService.java
@@ -1,26 +1,21 @@
 package kr.co.blockcom.board.biz.boardfree.service;
 
 import java.util.List;
-import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import kr.co.blockcom.board.biz.boardfree.mapper.BoardFreeMapper;
 import kr.co.blockcom.board.vo.board.BoardFree;
+import lombok.AllArgsConstructor;
 
 @Service
+@AllArgsConstructor
 public class BoardFreeService {
 	private static final Logger logger = LoggerFactory.getLogger(BoardFreeMapper.class);
 	
 	private final BoardFreeMapper boardFreeMapper;
-	
-	@Autowired
-	BoardFreeService(BoardFreeMapper boardFreeMapper){
-		this.boardFreeMapper=boardFreeMapper;
-	}
 	
 	public BoardFree selectPost(int seq) throws Exception{
 		logger.info("selectPost : {}", seq);
