1c0d5e67a64bec9524408d2efaa32f7a3b52cc11
kgh <rmsgud0202@blockcom.co.kr>
2018-11-16 11:50:34 +0900

    junit test...

--- src/test/java/kr/co/blockcom/board/service/BoardContorllerTest.java
+++ src/test/java/kr/co/blockcom/board/service/BoardContorllerTest.java
@@ -1,13 +1,30 @@
 package kr.co.blockcom.board.service;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
+import kr.co.blockcom.board.biz.boardfree.service.BoardFreeService;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
 class BoardContorllerTest {
+	
+	@Autowired
+	private BoardFreeService boardFreeService;
 	
 	@Test
 	void testWriteSubmit() {
-		fail("Not yet implemented");
+//		fail("Not yet implemented");
+		
+		System.out.println("test");
+		try {
+			System.out.println(boardFreeService.selectPost(58));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
